dccbf76e685724d00be57d17647a2bda2bfd367e
Vova Romanyuck <vovaromanyuck@MacBook-Air-Vova.local>
2024-03-08 21:10:46 +0200

    add api for fetch pharmacies and products_pharmacy

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-require("dotenv").config();
+const pharmacyController = require("./controllers/pharmacy.controller");
+const productController = require("./controllers/products.controller");
 
 var corsOptions = {
 	origin: "http://localhost:5173",
@@ -11,20 +12,15 @@ app.use(cors(corsOptions));
 
 app.use(express.json());
 
-const mysql = require("mysql2");
-
-const connection = mysql.createPool({
-	host: process.env.DB_HOST,
-	user: process.env.DB_USER,
-	password: process.env.DB_PASSWORD,
-	database: process.env.DB_NAME,
-});
-
 app.get("/hearbeat", (req, res) => {
 	res.json({ message: "Welcome to my application." });
 });
 
 const PORT = 8080;
+
+app.get("/pharmacies", pharmacyController.getPharmacies);
+
+app.get("/products/:pharmacyId", productController.getProductsByPharmacy);
 
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}.`);
