1c03d9b5db2c528122724093dc8f815a2884ca8e
Vova Romanyuck <vovaromanyuck@MacBook-Air-Vova.local>
2024-03-09 18:51:46 +0200

    add post and get methodts for order

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const app = express();
 const pharmacyController = require("./controllers/pharmacy.controller");
 const productController = require("./controllers/products.controller");
+const orderController = require("./controllers/orders.controller");
 
 var corsOptions = {
 	origin: "http://localhost:5173",
@@ -21,6 +22,9 @@ const PORT = 8080;
 app.get("/pharmacies", pharmacyController.getPharmacies);
 
 app.get("/products/:pharmacyId", productController.getProductsByPharmacy);
+
+app.get("/orders", orderController.getOrders);
+app.post("/order", orderController.createOrder);
 
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}.`);
